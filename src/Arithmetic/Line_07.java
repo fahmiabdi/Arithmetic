04b626dee32bcc1cefc0eac2b2bb7d5f84b14791
fahmiabdi <fahmiabdiputra@gmail.com>
2021-10-11 21:34:26 +0700

    Message First Push
    Signed-off-by: fahmiabdi <fahmiabdiputra@gmail.com>

--- src/Arithmetic/Line_07.java
+++ src/Arithmetic/Line_07.java
@@ -5,7 +5,26 @@ public class Line_07 {
 	// output = 2 -4 100 8 -10 100 -10 100 14
 	public static void main(String[] args) {
 		int n = 9;
-		
+		int idx = 1;
+		int idxGenap = 2;
+		int[] bilGenap = new int[n];
+
+		for (int i = 0; i < bilGenap.length; i++) {
+			if (idx == 1 || idx % 4 == 0) {
+				bilGenap[i] = -(idxGenap);
+			} else if (idx % 3 == 0) {
+				bilGenap[i] = 100;
+			} else {
+				bilGenap[i] = idxGenap;
+			}
+
+			idx += 1;
+			idxGenap += 2;
+		}
+
+		for (int j = 0; j < bilGenap.length; j++) {
+			System.out.printf("%5s", bilGenap[j]);
+		}
 
 	}
 
